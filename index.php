10fcefb29541fe2d1a250ede962b62767b543ba3
Felixvl <digitaleservice@gmail.com>
2018-03-09 14:57:21 +0100

    het dambord heeft de kleuren gekregen

--- index.php
+++ index.php
@@ -1,15 +1,51 @@
+<html>
+    <head>
+        <style>
+            td{
+                width: 40px;
+                height: 40px;
+            }
+            .wit{
+                background-color: white;
+            }
+            .zwart{
+                
+                background-color: black;
+            }
+        </style>
+    </head>
+    <body>
 <?php
 tabelmaken();
 
 function tabelmaken(){
     $hb = 10;
-    echo "<table border=1>";
+    echo "<table>";
     for($x=0;$x<$hb;$x++){
         echo "<tr>";
         for($y=0; $y<$hb;$y++){
-            echo "<td>a</td>";
+            if($x % 2){
+                echo bepaalkleur($y);
+            }else{
+                echo bepaalkleur($y,true);
+            }
         }
         echo "</tr>";
     }
     echo "</table>";
 }
+
+function bepaalkleur($positie, $omgekeerd = false){
+    if($omgekeerd){
+        $positie++;
+    }
+    if($positie % 2 == 0){
+        return "<td class=wit></td>";
+    }else{
+        return "<td class=zwart></td>";
+    }
+}
+
+?>
+    </body>
+</html>
